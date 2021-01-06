b87b8da40e6677af7f7dbaaa956acd2c84afba94
rynr <stevenpalta@gmail.com>
2021-01-05 19:32:23 -0500

    creación del proyecto

---
+++ practicaGit/src/main/java/practicaGit/Logica.java
@@ -0,0 +1,10 @@
+package practicaGit;
+
+public class Logica {
+	public String metodoIntegrante1() {
+		return "Integrante 1";
+	}
+	public String metodoIntegrante2() {
+		return "Integrante 2";
+	}
+}
---
+++ practicaGit/src/main/java/practicaGit/Principal.java
@@ -0,0 +1,11 @@
+package practicaGit;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Logica logi = new Logica();
+		System.out.println(logi.metodoIntegrante1());
+		Logica logi2 = new Logica();
+		System.out.println(logi2.metodoIntegrante2());
+	}
+}
