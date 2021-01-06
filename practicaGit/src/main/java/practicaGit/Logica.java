f576ad85636d0800155b1e1d534990a6508b162a
rynr <stevenpalta@gmail.com>
2021-01-05 19:41:43 -0500

    Método Integrante 1 modificado

--- practicaGit/src/main/java/practicaGit/Logica.java
+++ practicaGit/src/main/java/practicaGit/Logica.java
@@ -2,7 +2,7 @@ package practicaGit;
 
 public class Logica {
 	public String metodoIntegrante1() {
-		return "Integrante 1";
+		return " Este método ha sido modificado por Rayner Palta";
 	}
 	public String metodoIntegrante2() {
 		return "Integrante 2";
